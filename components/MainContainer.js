89caba4aa0cf594e41043bb3d1f1d13f8f117275
Nicole <nhswong@gmail.com>
2016-12-12 22:28:37 -0800

    add first component, MainContainer

---
+++ components/MainContainer.js
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react'
+import {
+	View,
+  Text,
+	StyleSheet,
+} from 'react-native'
+
+class MainContainer extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>BeBriefed App</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+})
+
+export default MainContainer
