8cdce3df3fa9c8833dd88cb085846242528e7873
Nicole <nhswong@gmail.com>
2016-12-13 13:49:50 -0800

    make TabBarItem component + add styling to the tabs

--- components/TabBarContainer.js
+++ components/TabBarContainer.js
@@ -1,17 +1,26 @@
-import React, {} from 'react'
+import React, { PropTypes } from 'react'
 import {
-  Text,
-  View,
 	StyleSheet,
 } from 'react-native'
 import Tabs from 'react-native-tabs'
 
+import TabBarItem from './TabBarItem'
+
+
 const TabBarContainer = (props) => (
-  <Tabs>
-    <Text>Web</Text>
-    <Text>DB</Text>
-    <Text>Mail</Text>
+  <Tabs style={styles.tabContainer}>
+    <TabBarItem name="web" label="Web Server" icon="server">Web</TabBarItem>
+    <TabBarItem name="db" label="DB Server" icon="database">DB</TabBarItem>
+    <TabBarItem name="mail" label="Mail Server" icon="envelope-o">Mail</TabBarItem>
   </Tabs>
 )
 
+const styles = StyleSheet.create({
+	tabContainer: {
+		backgroundColor: '#343434',
+		borderTopWidth: 1,
+		borderTopColor: '#262626',
+		height: 96,
+	},
+})
 export default TabBarContainer
